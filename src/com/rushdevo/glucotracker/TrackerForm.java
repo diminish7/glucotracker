2fb2d3965a880f2afb2434574a11205be816542d
Jason Rush <diminish7@gmail.com>
2011-08-12 13:17:22 -0600

    initial commit

---
+++ src/com/rushdevo/glucotracker/TrackerForm.java
@@ -0,0 +1,13 @@
+package com.rushdevo.glucotracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TrackerForm extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tracker_form);
+    }
+}
