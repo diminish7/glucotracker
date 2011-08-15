ee8e3d59f2fdae5616b452cd9d467fa5fd9a0bc8
Jason Rush <diminish7@gmail.com>
2011-08-15 12:42:54 -0600

    Added date and time picker

--- gen/com/rushdevo/glucotracker/R.java
+++ gen/com/rushdevo/glucotracker/R.java
@@ -26,10 +26,8 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int blood_sugar=0x7f040001;
-        public static final int blood_sugar_correction=0x7f040005;
-        public static final int blood_sugar_date=0x7f040002;
-        public static final int blood_sugar_meal=0x7f040004;
-        public static final int blood_sugar_time=0x7f040003;
+        public static final int blood_sugar_correction=0x7f040003;
+        public static final int blood_sugar_meal=0x7f040002;
     }
     public static final class style {
         public static final int FormLabel=0x7f050000;
